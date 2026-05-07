06d2e263a1592a8e4f8e33d97cfd654cb05f3df2
yarty777 <yaroslav0908l@gmail.com>
2026-05-07 17:57:22 +0300

    Fixed cors

--- kurswork_back/Program.cs
+++ kurswork_back/Program.cs
@@ -12,7 +12,10 @@ builder.Services.AddCors(options =>
     options.AddPolicy("ReactCorsPolicy", policy =>
     {
         policy
-            .WithOrigins("http://localhost:5173")
+            .WithOrigins(
+                "http://localhost:5173",
+                "https://kurswork.vercel.app"
+            )
             .AllowAnyHeader()
             .AllowAnyMethod()
             .AllowCredentials();
