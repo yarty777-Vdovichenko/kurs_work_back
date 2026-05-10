72173f7efe57d92e717ef1266048cd467e804efd
yarty777 <yaroslav0908l@gmail.com>
2026-05-10 20:43:53 +0300

    Access token

--- kurswork_back/obj/Debug/net8.0/kurswork_back.AssemblyInfo.cs
+++ kurswork_back/obj/Debug/net8.0/kurswork_back.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("kurswork_back")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ae081ee0c0fcb69e4374c711bdb770b415c0e0a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e20c0e55eb320f67f93ff46d64b48b1bdab5d17")]
 [assembly: System.Reflection.AssemblyProductAttribute("kurswork_back")]
 [assembly: System.Reflection.AssemblyTitleAttribute("kurswork_back")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
